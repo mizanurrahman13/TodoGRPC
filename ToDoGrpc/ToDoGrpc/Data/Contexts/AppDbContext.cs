267992b45a95b68ff72edb17b7337d71bd4f130d
mizanurrahman13 <mizan.441980@gmail.com>
2025-01-23 04:29:35 +0600

    Added ToDo CRUD with Grpc and Http

---
+++ ToDoGrpc/ToDoGrpc/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoGrpc.Models;
+
+namespace ToDoGrpc.Data.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+}
